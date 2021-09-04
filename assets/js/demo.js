d04c7e81cf15739a2b8e714ec54c3ed9b7142c53
adi <47937083+wewanadi@users.noreply.github.com>
2021-09-04 14:11:58 +0800

    45system

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -11,24 +11,24 @@ $(function() {
         if (pwd==="@1019873212333"&&id==="101987321"){
       var tl = new TimelineMax();
       tl.fromTo(passwordDiv, 0.3, {x:-1}, { x:1, ease:RoughEase.ease.config({ strength:8, points:40, template:Linear.easeNone, randomize:false }) , clearProps:"x" })
-        .to($('body'), 0.15, { backgroundColor: '#E8FFC4	' })
+        .to($('body'), 0.15, { backgroundColor: 'green' })
         .to(errorMessage, 0.15, { autoAlpha: 1, y: -16 }, "-=0.15")
         // .to(passwordDiv, 0.15, { className: "+=false" }, "-=0.15")
         .to(passwordDiv, 0.15, { className: "-=false" }, "+=2.5")
-        .to($('body'), 0.15, { backgroundColor: '#F1E1FF' }, "+=0.65")
+        .to($('body'), 0.15, { backgroundColor: '#EDF0F9' }, "+=0.65")
         .to(errorMessage, 0.15, { autoAlpha: 0, y: 0 }, "-=0.15");
         msg.textContent = "Correct, Loading...";
-        setTimeout(() => {          window.location.href = "https://sites.google.com/view/nthurationing-plan-101987321";}, 1500);
+        setTimeout(() => {          window.location.href = "https://sites.google.com/view/nthurationing-101987321";}, 1500);
 
         }
         else{
           var tl = new TimelineMax();
           tl.fromTo(passwordDiv, 0.3, {x:-1}, { x:1, ease:RoughEase.ease.config({ strength:8, points:40, template:Linear.easeNone, randomize:false }) , clearProps:"x" })
-            .to($('body'), 0.15, { backgroundColor: '#FFD2D2' })
+            .to($('body'), 0.15, { backgroundColor: '#E74C3C' })
             .to(errorMessage, 0.15, { autoAlpha: 1, y: -16 }, "-=0.15")
             .to(passwordDiv, 0.15, { className: "+=false" }, "-=0.15")
             .to(passwordDiv, 0.15, { className: "-=false" }, "+=2.5")
-            .to($('body'), 0.15, { backgroundColor: '#F1E1FF' }, "+=0.65")
+            .to($('body'), 0.15, { backgroundColor: '#EDF0F9' }, "+=0.65")
             .to(errorMessage, 0.15, { autoAlpha: 0, y: 0 }, "-=0.15");
         }
     }
